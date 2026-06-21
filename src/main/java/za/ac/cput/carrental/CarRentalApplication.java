b872078340d8d154c7aa00cff175fc89beae89ca
owenj <223219665@mycput.ac.za>
2026-06-21 13:51:01 +0200

    domain

---
+++ src/main/java/za/ac/cput/carrental/CarRentalApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/za/ac/cput/carrental/domain/Member.java
@@ -0,0 +1,4 @@
+package za.ac.cput.carrental.domain;
+
+public class Member {
+}
